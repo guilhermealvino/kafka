d2b2dafe1d0a7098ee3c0c384da5319a85c77a0d
guilhermealvino <guilhermealvi9@gmail.com>
2021-10-16 06:34:26 -0300

    Producer and consumer using kafka with docker

---
+++ consumer/src/main/java/br/com/guilherme/kafka/consumer/listener/ConsumerBoraPratricar.java
@@ -0,0 +1,19 @@
+package br.com.guilherme.kafka.consumer.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class ConsumerBoraPratricar {
+
+    private final Logger logger = LoggerFactory.getLogger(ConsumerBoraPratricar.class);
+
+    @KafkaListener(topics = "${topic.guilhermeconsumer}", groupId = "group_id")
+    public void consume(String message) throws IOException{
+        logger.info(String.format("#### -> Consumed message -> %s", message));
+    }
+}
